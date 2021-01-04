da60dd60546aae439301c1109e73bba591034706
Jared Clark <rjaredclark@gmail.com>
2021-01-03 22:05:03 -0600

    added argparse to pick player types

--- run_game.py
+++ run_game.py
@@ -1,9 +1,43 @@
+import argparse
 from game_core import GameCore
 from player_terminal import PlayerTerminal
 from player_rule import PlayerRule
 
+PLAYER_TERMINAL_KEY = 'terminal'
+PLAYER_RULE_KEY = 'rule'
+VALID_PLAYERS = [PLAYER_TERMINAL_KEY, PLAYER_RULE_KEY]
+
+
+
+'''
+setup and call an argparse object. 
+return: args object from evaluating an argparse object (parse_args())
+'''
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p1', required=True, choices=VALID_PLAYERS,
+        help='The type of player to set as player 1')
+    parser.add_argument('-p2', required=True, choices=VALID_PLAYERS,
+        help='The type of player to set as player 2')
+    return parser.parse_args()
+
+
+
+'''
+return the proper player type based on the passed string
+'''
+def create_player(player_type):
+    if player_type == PLAYER_TERMINAL_KEY:
+        return PlayerTerminal()
+    elif player_type == PLAYER_RULE_KEY:
+        return PlayerRule()
+    raise ValueError(f'player_type of value "{player_type}" is not supported')
+
+
+
 if __name__ == '__main__':
-    player_1 = PlayerTerminal()
-    player_2 = PlayerRule()
+    args = get_args()
+    player_1 = create_player(args.p1)
+    player_2 = create_player(args.p2)
     game = GameCore(player_1, player_2)
     game.run()
