6ae5e2a30ceeaa3ea89ef2ad9d32d4345d3b6ca2
Jared Clark <rjaredclark@gmail.com>
2021-01-15 23:25:36 -0600

    modifications for RL player (incomplete)

--- run_game.py
+++ run_game.py
@@ -3,12 +3,14 @@ from game_core import GameCore
 from player_terminal import PlayerTerminal
 from player_rule import PlayerRule
 from player_random import PlayerRandom
+from player_rl import PlayerRL
 
 
 PLAYER_TERMINAL_KEY = 'terminal'
 PLAYER_RULE_KEY = 'rule'
 PLAYER_RANDOM_KEY = 'random'
-VALID_PLAYERS = [PLAYER_TERMINAL_KEY, PLAYER_RULE_KEY, PLAYER_RANDOM_KEY]
+PLAYER_RL_KEY = 'rl'
+VALID_PLAYERS = [PLAYER_TERMINAL_KEY, PLAYER_RULE_KEY, PLAYER_RANDOM_KEY, PLAYER_RL_KEY] 
 
 
 
@@ -22,20 +24,32 @@ def get_args():
         help='The type of player to set as player 1')
     parser.add_argument('-p2', required=True, choices=VALID_PLAYERS,
         help='The type of player to set as player 2')
+    parser.add_argument('-r', '--rounds', type=int, default=1, 
+        help='number of rounds to play (int). Default is 1. ' + 
+            'Log will not be saved if default is used.')
+    parser.add_argument('-l', '--learn', action='store_true',
+        help='indicate if the RL agent should learn')
+    parser.add_argument('-e', '--explore', type=float, default=0.2,
+        help='explore parameter for the RL agent')
     return parser.parse_args()
 
 
 
 '''
+params:
+    player_type: (string) the player type to create
+    args: argparse params that can be passed to players
 return the proper player type based on the passed string
 '''
-def create_player(player_type):
+def create_player(player_type, args):
     if player_type == PLAYER_TERMINAL_KEY:
         return PlayerTerminal()
     elif player_type == PLAYER_RULE_KEY:
         return PlayerRule()
     elif player_type == PLAYER_RANDOM_KEY:
         return PlayerRandom()
+    elif player_type == PLAYER_RL_KEY:
+        return PlayerRL(args.learn, args.explore)
     raise ValueError(f'player_type of value "{player_type}" is not supported')
 
 
@@ -46,10 +60,18 @@ if __name__ == '__main__':
     player_2 = None
     if args.p1 == args.p2:
         # use the same player instance if they are the same type
-        player_1 = create_player(args.p1)
+        player_1 = create_player(args.p1, args)
         player_2 = player_1 
     else:
-        player_1 = create_player(args.p1)
-        player_2 = create_player(args.p2)
-    game = GameCore(player_1, player_2)
-    game.run()
+        player_1 = create_player(args.p1, args)
+        player_2 = create_player(args.p2, args)
+    
+    # TODO add loading of existing Q function
+    
+    game = None
+    for i in range(args.rounds):
+        game = GameCore(player_1, player_2)
+        game.run()
+
+    # TODO log arguments (pickle)
+    # TODO add saving of Q function
