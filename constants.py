e09cc310825f3c6ccfc59ec3412acbdf76934b46
Jared Clark <rjaredclark@gmail.com>
2021-01-15 23:23:02 -0600

    added indices of winning pairs of 3

--- constants.py
+++ constants.py
@@ -3,3 +3,14 @@
 CELL_EMPTY = ' '
 CELL_PLAYER_1 = 'X'
 CELL_PLAYER_2 = 'O'
+
+WIN_OPTIONS = [
+    [[0,0], [0,1], [0,2]],  # top row
+    [[1,0], [1,1], [1,2]],  # middle row
+    [[2,0], [2,1], [2,2]],  # bottom row
+    [[0,0], [1,0], [2,0]],  # left column
+    [[0,1], [1,1], [2,1]],  # middle column
+    [[0,2], [1,2], [2,2]],  # right column
+    [[0,0], [1,1], [2,2]],  # top-left to bottom-right diag
+    [[0,2], [1,1], [2,0]]   # top-right to bottom-left diag
+]
